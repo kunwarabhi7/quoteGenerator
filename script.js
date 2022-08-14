cccab9eec88d987ee2ec674146c0eddd406e45f0
Abhishek <abhishekparmar162@gail.com>
2022-08-14 18:58:19 +0530

    added Loader Icon

--- script.js
+++ script.js
@@ -3,15 +3,31 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterButton = document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 
 
 let apiQuotes = [];
 
+//Loading Show
+
+const loading = () => {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//hide loading
+
+const complete = () => {
+    quoteContainer.hidden = false
+    loader.hidden = true
+}
+
 //show new Quote
 
 const newQuote = () => {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length) + 1]
     if (!quote.author) {
         authorText.textContent = 'Unknown';
@@ -27,6 +43,7 @@ const newQuote = () => {
     }
 
     quoteText.textContent = quote.text;
+    complete();
 
 
 }
@@ -35,6 +52,7 @@ const newQuote = () => {
 //Get Quotes from API
 
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl)
