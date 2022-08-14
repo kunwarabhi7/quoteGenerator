89970400ba425a59e13ebbc0a48169fa43f25ca5
Abhishek <abhishekparmar162@gail.com>
2022-08-14 18:26:24 +0530

    Display quote and author from api

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
+
+
+
+
 let apiQuotes = [];
+
+//show new Quote
+
+const newQuote = () => {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length) + 1]
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
+
+}
+
 //Get Quotes from API
 
 async function getQuotes() {
@@ -6,8 +25,7 @@ async function getQuotes() {
     try {
         const response = await fetch(apiUrl)
         apiQuotes = await response.json();
-        console.log(apiQuotes);
-
+        newQuote();
     } catch (error) {
         alert(error)
     }
