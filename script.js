53feea44d15cec1b64a2473967782f979f1d10bd
Abhishek <abhishekparmar162@gail.com>
2022-08-14 18:09:56 +0530

    fetch the quote generator api

---
+++ script.js
@@ -0,0 +1,19 @@
+let apiQuotes = [];
+//Get Quotes from API
+
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl)
+        apiQuotes = await response.json();
+        console.log(apiQuotes);
+
+    } catch (error) {
+        alert(error)
+    }
+}
+
+//onLoad
+getQuotes();
+
+
